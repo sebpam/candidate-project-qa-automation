c3568f5dfa73fac6732395e7776bb1c52d2746c9
sebpam <pambu76@hotmail.com>
2023-04-13 14:35:15 -0400

    comment out google map tests -- incomplete

--- validations/GoogleMapTests.ts
+++ validations/GoogleMapTests.ts
@@ -3,20 +3,20 @@ import { expect } from "chai";
 const schedule = new Schedule();
 import * as moment from "moment";
 
-class ScheduleTests {
-  scenario;
-  constructor(scenario) {
-    this.scenario = scenario;
-  }
-  navigateToSchedulingPage() {
-    it(":Should validate the title display", async () => {
-      await schedule.waitForThisPresence(schedule.cityDropdown, 10000);
-      expect((await schedule.pageTitle()).replace(/\s/g, "")).to.equal(
-        "Schedule  | ZoomCare".replace(/\s/g, "")
-      );
-    });
-  }
-  validateDefaultSchedulePageDisplays() {}
+class GoogleMapTests {
+  // scenario;
+  // constructor(scenario) {
+  //   this.scenario = scenario;
+  // }
+  // navigateToSchedulingPage() {
+  //   it(":Should validate the title display", async () => {
+  //     await schedule.waitForThisPresence(schedule.cityDropdown, 10000);
+  //     expect((await schedule.pageTitle()).replace(/\s/g, "")).to.equal(
+  //       "Schedule  | ZoomCare".replace(/\s/g, "")
+  //     );
+  //   });
+  // }
+  // validateDefaultSchedulePageDisplays() {}
 }
 
-export default ScheduleTests;
+export default GoogleMapTests;
